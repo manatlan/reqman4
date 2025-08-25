4b7b5a16e0d5bf3ce4bfc6986c53aeba25aca2c1
manatlan <manatlan@gmail.com>
2025-08-25 18:43:43 +0200

    btter

--- src/request.py
+++ src/request.py
@@ -49,62 +49,63 @@ class ResponseInvalid(ResponseError):
 async def call(method, url:str,body:bytes|None=None, headers:httpx.Headers = httpx.Headers(), timeout:int=60_000, proxies=None) -> httpx.Response:
     logger.debug(f"CALL {method} {url} with body={body} headers={headers} timeout={timeout} proxies={proxies}")
     # Simule une réponse HTTP
-    if method == "GET" and url.startswith("/testdict"):
-        return httpx.Response(
-            status_code=200,
-            headers={"content-type": "application/json"},
-            json={
-                "coco": "VAL",
-                "liste": [1, 2, {"key": "val"}]
-            },
-            request=httpx.Request(method, url, headers=headers, content=body and str(body))
-        )
-    elif method == "GET" and url.startswith("/testlist"):
-        return httpx.Response(
-            status_code=200,
-            headers={"content-type": "application/json"},
-            json=[1, 2, {"key": "val"}]
-            ,
-            request=httpx.Request(method, url, headers=headers, content=body and str(body))
-        )
-    elif method == "POST" and url.startswith("/test"):
-        return httpx.Response(
-            status_code=201,
-            headers={"content-type": "application/json"},
-            json=dict(result=body),
-            request=httpx.Request(method, url, headers=headers, content=body and str(body))
-        )
+    if url.startswith("http://test"):
+        if method == "GET" and url.startswith("http://test/testdict"):
+            return httpx.Response(
+                status_code=200,
+                headers={"content-type": "application/json"},
+                json={
+                    "coco": "VAL",
+                    "liste": [1, 2, {"key": "val"}]
+                },
+                request=httpx.Request(method, url, headers=headers, content=body and str(body))
+            )
+        elif method == "GET" and url.startswith("http://test/testlist"):
+            return httpx.Response(
+                status_code=200,
+                headers={"content-type": "application/json"},
+                json=[1, 2, {"key": "val"}]
+                ,
+                request=httpx.Request(method, url, headers=headers, content=body and str(body))
+            )
+        elif method == "POST" and url.startswith("http://test/test"):
+            return httpx.Response(
+                status_code=201,
+                headers={"content-type": "application/json"},
+                json=dict(result=body),
+                request=httpx.Request(method, url, headers=headers, content=body and str(body))
+            )
+        else:
+            return httpx.Response(
+                status_code=404,
+                headers={"content-type": "text/plain"},
+                content=b"Not Found",
+                request=httpx.Request(method, url, headers=headers, content=body and str(body))
+            )
     else:
-        return httpx.Response(
-            status_code=404,
-            headers={"content-type": "plain/text"},
-            content="not found"
-            ,
-            request=httpx.Request(method, url, headers=headers, content=body and str(body))
-        )
 
+        assert method in KNOWNVERBS, f"Unknown HTTP verb {method}"
+        try:
 
-    assert method in KNOWNVERBS, f"Unknown HTTP verb {method}"
-    try:
+            # AHTTP._get_proxy_map(proxies, False)
+            AHTTP = httpx.AsyncClient(follow_redirects=True,verify=False,cookies=jar)
 
-        AHTTP._get_proxy_map(proxies, False)
+            return await AHTTP.request(
+                method,
+                url,
+                data=body,
+                headers=headers,
+                timeout=timeout,   # sec to millisec
+            )
 
-        return await AHTTP.request(
-            method,
-            url,
-            data=body,
-            headers=headers,
-            timeout=timeout,   # sec to millisec
-        )
+            # info = "%s %s %s" % (r.http_version, int(r.status_code), r.reason_phrase)
 
-        # info = "%s %s %s" % (r.http_version, int(r.status_code), r.reason_phrase)
-
-    except (httpx.TimeoutException):
-        return ResponseTimeout()
-    except (httpx.ConnectError):
-        return ResponseUnreachable()
-    except (httpx.InvalidURL,httpx.UnsupportedProtocol,ValueError):
-        return ResponseInvalid(url)
+        except (httpx.TimeoutException):
+            return ResponseTimeout()
+        except (httpx.ConnectError):
+            return ResponseUnreachable()
+        except (httpx.InvalidURL,httpx.UnsupportedProtocol,ValueError):
+            return ResponseInvalid(url)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
