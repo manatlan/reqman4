8bead80083a16d52a5b28e7f7c42e54c3c2b7b00
manatlan <manatlan@gmail.com>
2025-09-11 13:51:51 +0200

    tnr

--- test_scenario.py
+++ test_scenario.py
@@ -51,8 +51,7 @@ def test_no_reqman_conf():
 @pytest.mark.parametrize("example_file", sorted(glob.glob("examples/ok/*.yml")) )
 async def test_scenarios_ok(example_file):
 
-    if "compat" not in example_file:
-        assert validate_yaml(example_file,"schema.json")
+    assert validate_yaml(example_file,"schema.json")
 
 
     s=scenario.Scenario(example_file)
@@ -69,21 +68,24 @@ async def test_scenarios_ok(example_file):
 @pytest.mark.parametrize("example_file", sorted(glob.glob("examples/err/*.yml")) )
 async def test_scenarios_err(example_file):
     #assert not validate_yaml(example_file,"schema.json")
-
-    with open(example_file, "r") as f:
-        first_line = f.readline().strip()
-    assert first_line.startswith("#ERROR:")
-    error_message = first_line[len("#ERROR:"):].strip()
+    
+    good,error_message = attendings(example_file)
 
     with pytest.raises(Exception) as excinfo:
         s=scenario.Scenario(example_file)
         async for echange in s.execute():
-            ...
+                ...
+    assert not good
     assert error_message in str(excinfo.value)
 
 @pytest.mark.parametrize("example_file", sorted(glob.glob("examples/ko/*.yml")) )
 def test_scenarios_ko(example_file):
     simulate(example_file)
+
+@pytest.mark.parametrize("example_file", sorted(glob.glob("examples/reqman3/*.yml")) )
+def test_scenarios_compat(example_file):
+    simulate(example_file)
+
 
 
 @pytest.mark.skipif(os.getenv("CI") == "true", reason="No internet on CI")
